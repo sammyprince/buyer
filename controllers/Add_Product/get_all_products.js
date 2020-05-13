53ec7ccf4d8ce10dc91dd4e2a8030e0c4ab0fc82
Hafiz Aqib <hafizaqib22@gmail.com>
2020-05-14 00:51:12 +0500

    make it better

--- controllers/Add_Product/get_all_products.js
+++ controllers/Add_Product/get_all_products.js
@@ -1,10 +1,14 @@
 const _ = require('lodash');
 const {RENDER_BAD_REQUEST} = require('../common/utils');
 const {Add_Product} = require('../../models/add_product');
+const {User} = require('../../models/user');
 
 const list = async (req, res) => {
     try {
-        const _products = await Add_Product.getAllList();
+        const token = req.header('x-sh-auth');
+        const user = await User.findByToken(token);
+
+        const _products = await Add_Product.getAllByUser(user._id);
 
         if (!_products) {
             res.status(400).json({ message : "not found"});
